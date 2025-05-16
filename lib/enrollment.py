dab15cfe4365a59f2d57868d5e5518502c073ddd
Esther-Sonia <esthersonia21@gmail.com>
2025-05-16 10:28:36 +0300

    add aggregate methods for student and enrollment classes

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}  # added a new attribute to store grades
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -15,9 +17,25 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def assign_grade(self, enrollment, grade):
+        if enrollment in self._enrollments:
+            self._grades[enrollment] = grade
+        else:
+            raise ValueError("Enrollment not found for this student")
+
+    def aggregate_average_grade(self):
+        if not self._grades:
+            return 0  
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        return total_grades / num_courses
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -33,7 +51,7 @@ class Course:
 
 class Enrollment:
     all = []
-    
+
     def __init__(self, student, course):
         if isinstance(student, Student) and isinstance(course, Course):
             self.student = student
@@ -45,3 +63,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
